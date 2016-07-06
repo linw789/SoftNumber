24effdfc23d84d99600dcc32f4bfb78e77171132
Lin Wang <lwang@machinezone.com>
2016-07-06 14:27:34 -0700

    unroll the adder loop

--- soft_number.h
+++ soft_number.h
@@ -89,13 +89,51 @@ SU32 DiminishedTwoComplement(SU32 x)
 
 SU32 operator+(SU32 x, SU32 y)
 {
+    U32 bit_x = 0, bit_y = 0;
     U32 sum = 0;
     U32 carry = 0;
     U32 mask = 1;
+    
     for (;;)
     {
-        U32 bit_x = x.v & mask;
-        U32 bit_y = y.v & mask;
+        // unroll the loop a bit
+        
+        // 1
+        bit_x = x.v & mask;
+        bit_y = y.v & mask;
+        sum |= (bit_x ^ bit_y) ^ carry;
+        
+        mask = mask << 1;
+        if (!mask) break;
+        
+        carry = (bit_x & bit_y) | ((bit_x ^ bit_y) & carry);
+        carry = carry << 1;
+        
+        // 2
+        bit_x = x.v & mask;
+        bit_y = y.v & mask;
+        sum |= (bit_x ^ bit_y) ^ carry;
+        
+        mask = mask << 1;
+        if (!mask) break;
+        
+        carry = (bit_x & bit_y) | ((bit_x ^ bit_y) & carry);
+        carry = carry << 1;
+        
+        // 3
+        bit_x = x.v & mask;
+        bit_y = y.v & mask;
+        sum |= (bit_x ^ bit_y) ^ carry;
+        
+        mask = mask << 1;
+        if (!mask) break;
+        
+        carry = (bit_x & bit_y) | ((bit_x ^ bit_y) & carry);
+        carry = carry << 1;
+        
+        // 4
+        bit_x = x.v & mask;
+        bit_y = y.v & mask;
         sum |= (bit_x ^ bit_y) ^ carry;
         
         mask = mask << 1;
